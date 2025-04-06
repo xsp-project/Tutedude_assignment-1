84c04cd40f89887d5e3378e62fef85e3e54a9402
Sachin pandit <130636101+xsp-project@users.noreply.github.com>
2025-04-06 16:56:01 +0530

    Add files via upload

---
+++ Task_2.py
@@ -0,0 +1,7 @@
+First_name = input("Enter your first name: ")
+Last_name = input("Enter your last name: ")
+
+def Greeting (First_name , Last_name):
+    print(f"Hello {First_name} {Last_name}, welcome to my Github Repository!")
+    
+Greeting(First_name , Last_name)
