ee925aa0f1efb45f25a94cdaaaff2a7ff66c7dd8
Sachin pandit <130636101+xsp-project@users.noreply.github.com>
2025-04-06 16:36:15 +0530

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,11 @@
+Num_1 = int(input("Enter 1st number!: "))
+Num_2 = int(input("Enter 2nd number!: "))
+
+def mathematical_operations (Num_1 , Num_2):
+    
+    print(f"Addition of 1st and 2nd number is : {Num_1 + Num_2}")
+    print(f"Subtraction of 1st and 2nd number is : {Num_1 - Num_2}")
+    print(f"Multiplication of 1st and 2nd number is : {Num_1 * Num_2}")
+    print(f"Division of 1st and 2nd number is : {Num_1 / Num_2}")
+    
+mathematical_operations(Num_1 , Num_2)
